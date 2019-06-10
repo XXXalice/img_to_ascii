c3439787177c49cc46a58f94fb2f64b3d5346deb
梅宮 <euthanasia045@gmail.com>
2019-06-10 18:28:35 +0900

    first complete aa

--- _preparation/test.py
+++ _preparation/test.py
@@ -1,10 +1,18 @@
-from handler import encoder
-from handler import logger
+# from handler import encoder
+# from handler import logger
+#
+#
+# print('-------------------')
+# enc = encoder.Encoder()
+# print('-------------------')
+# log = logger.Logger('.aalog')
+# trace = log._trace_test()
+# print(trace[1])
+#
+# from PIL import Image
+# img = Image.open('../strage/test.jpg')
+# img = img.convert('L')
+# img = img.resize((100, 100))
+# img.show()
 
-
-print('-------------------')
-enc = encoder.Encoder()
-print('-------------------')
-log = logger.Logger('.aalog')
-trace = log._trace_test()
-print(trace[1])
+print(dir())
--- exe.py
+++ exe.py
@@ -1,4 +1,13 @@
+import argparse
 from handler.encoder import Encoder
 
+parser = argparse.ArgumentParser(description='がぞーかんじへんかん')
+parser.add_argument('-i', '--image', help='変換する画像')
+parser.add_argument('-f', '--fineness', help='処理の細かさ（デフォ横50文字）')
+args = parser.parse_args()
+
 enc = Encoder()
-enc.upload('./test.jpg')
+enc.upload(args.image)
+img = enc.preprocess('resize_gray', aa_width=int(args.fineness))
+cchar_art = enc.img_2_cchar(img)
+print(cchar_art)
