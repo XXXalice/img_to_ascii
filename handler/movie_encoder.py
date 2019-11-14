c7f9de5def9fc8eb46c61c1873b03a2f2fd03b91
梅宮 <euthanasia045@gmail.com>
2019-11-14 15:32:20 +0900

    WIP movie_encoder 動画読み込みのpathを改善

--- handler/movie_encoder.py
+++ handler/movie_encoder.py
@@ -1,3 +1,4 @@
+import os
 from .encoder import Encoder
 import cv2
 
@@ -7,8 +8,9 @@ class MovieEncoder(Encoder):
         self.fps = fps
 
     def show_movie(self, fpath):
+        movie_path = os.path.join(self.strage_path, fpath)
         try:
-            cap = cv2.VideoCapture(fpath)
+            cap = cv2.VideoCapture(movie_path)
             while(cap.isOpened()):
                 ret, frame = cap.read()
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -21,3 +23,4 @@ class MovieEncoder(Encoder):
         finally:
             cap.release()
             cv2.destroyAllWindows()
+
